f94d6096fcadba0cb34e474f0e0a4aa34b72bb91
sleepydogo <tomaschattmann@gmail.com>
2025-12-19 17:46:35 -0300

    fix: remove hardware wallets support

--- frontend/components/wallet/WalletProvider.tsx
+++ frontend/components/wallet/WalletProvider.tsx
@@ -3,7 +3,8 @@
 import { useMemo } from 'react';
 import { ConnectionProvider, WalletProvider as SolanaWalletProvider } from '@solana/wallet-adapter-react';
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
-import { PhantomWalletAdapter, SolflareWalletAdapter } from '@solana/wallet-adapter-wallets';
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-phantom';
+import { SolflareWalletAdapter } from '@solana/wallet-adapter-solflare';
 import { clusterApiUrl } from '@solana/web3.js';
 
 require('@solana/wallet-adapter-react-ui/styles.css');
